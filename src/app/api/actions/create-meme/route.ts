4997c4e840192d9cbd31f0098b37c6d70d2c80ef
FrozenArcher <yhy242989105@hotmail.com>
2024-12-05 01:40:22 +0800

    feat: action chaining

--- src/app/api/actions/create-meme/route.ts
+++ src/app/api/actions/create-meme/route.ts
@@ -129,6 +129,18 @@ export const POST = async (req: Request) => {
         transaction,
         message: "Meme created",
         type: "transaction",
+        links: {
+          next: {
+            type: "inline",
+            action: {
+              icon: "https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/",
+              label: "Create",
+              title: "Create token account",
+              type: "action",
+              description: "Create an account for the token",
+            }
+          },
+        },
       },
     });
 
