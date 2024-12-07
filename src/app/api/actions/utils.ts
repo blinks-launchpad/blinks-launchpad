3fd5b45b19180f5d6ffe87a0b11cdeb7c2a658c8
FrozenArcher <yhy242989105@hotmail.com>
2024-12-07 14:47:52 +0800

    feat: update base url

--- src/app/api/actions/utils.ts
+++ src/app/api/actions/utils.ts
@@ -10,7 +10,7 @@ export const iconUrl =
   "https://pbs.twimg.com/profile_images/1864314913881247749/8Hpvmc43.jpg";
 
 export const actionHeaders = createActionHeaders({
-  chainId: "devnet", // or chainId: "devnet"
+  chainId: "mainnet-beta", // or chainId: "devnet"
   actionVersion: "2.2.1", // the desired spec version
 });
 
@@ -28,7 +28,8 @@ export const actionErrorResponse = (message: string) => {
 
 export const getElizaUrl = (path: string) => {
   if (!path.startsWith("/")) path = `/${path}`;
-  return `http://localhost:3000${path}`;
+  const baseUrl = process.env.NEXT_PUBLIC_ELIZA_BASE_URL;
+  return `${baseUrl}${path}`;
 };
 
 export const getProvider = () => {
